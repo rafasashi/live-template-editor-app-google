82fc039a25c9cda4d99d5d2d9cbdb473c948b07d
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2022-03-16 14:47:47 +0100

    app class updated

--- includes/class-ltple.php
+++ includes/class-ltple.php
@@ -2,7 +2,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class LTPLE_App_Google {
+class LTPLE_App_Google extends LTPLE_Client_App {
 
 	/**
 	 * The single instance of LTPLE_App_Google.
--- live-template-editor-app-google.php
+++ live-template-editor-app-google.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Live Template Editor App Google
- * Version: 1.0
+ * Version: 1.0.0.2
  * Plugin URI: https://github.com/rafasashi
  * Description: Google.com API integrator for Live Template Editor.
  * Author: Rafasashi
